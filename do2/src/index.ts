ba7a3e26bd6559238b8a1bc0d8941d8fe499449c
jldec <jurgen@jldec.me>
2024-09-22 13:27:37 +0100

    import type for DO class

--- do2/src/index.ts
+++ do2/src/index.ts
@@ -1,16 +1,14 @@
-import { DurableObject } from 'cloudflare:workers'
+import type { MyDurableObject } from '../../do1/src'
 
 export interface Env {
-  MY_DURABLE_OBJECT: DurableObjectNamespace
+  MY_DURABLE_OBJECT: DurableObjectNamespace<MyDurableObject>
 }
 
 export default {
   async fetch(request, env, ctx): Promise<Response> {
     let id: DurableObjectId = env.MY_DURABLE_OBJECT.idFromName(new URL(request.url).pathname)
     let client = env.MY_DURABLE_OBJECT.get(id)
-		// @ts-ignore
     if (request.method === 'POST') return new Response('' + (await client.bump()))
-    // @ts-ignore
     return new Response('' + (await client.get()))
   },
 } satisfies ExportedHandler<Env>
